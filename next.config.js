5a55a65c22af7df916ede3c397af75d596915903
sabishii-bit <gmhlg@live.com>
2025-03-26 20:48:07 -0700

    Added vercel.json

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Don't use 'export' output for Vercel deployment
+  // This was causing the routes-manifest.json to not be generated
+};
+
+module.exports = nextConfig; 
